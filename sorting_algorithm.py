f707b0e7676a41679d2ae3a35bb15a0ac4d23d81
Punit kashyap <95774812+Punit-kashyap-projects@users.noreply.github.com>
2022-05-17 22:57:21 +0530

    Update sorting_algorithm.py

--- sorting_algorithm.py
+++ sorting_algorithm.py
@@ -4,10 +4,10 @@ arr = [2, 4, 5, 5434, 53, 98, 7, 4, 34]
 # functioning having sorting algorithm
 def sortArr(arr):
     newArr = []
-    newArr.append(arr.pop(arr.index(minVal(arr))))
+    newArr.append(arr.pop(arr.index(min(arr))))
 
     while len(arr) > 0:
-        newArr.append(arr.pop(arr.index(minVal(arr))))
+        newArr.append(arr.pop(arr.index(min(arr))))
 
     return newArr
 
