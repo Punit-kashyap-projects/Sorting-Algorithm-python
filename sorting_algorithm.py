bd3b4ced7ec40ac7021367df8bb92e17b1b505a6
Punit kashyap <95774812+Punit-kashyap-projects@users.noreply.github.com>
2022-05-17 22:56:08 +0530

    Create sorting_algorithm.py

---
+++ sorting_algorithm.py
@@ -0,0 +1,15 @@
+# target array to sort
+arr = [2, 4,5,5434,53,98,7,4,34]
+
+# functioning having sorting algorithm
+def sortArr(arr):
+    newArr = []
+    newArr.append(arr.pop(arr.index(minVal(arr))))
+
+    while len(arr) > 0:
+        newArr.append(arr.pop(arr.index(minVal(arr))))
+
+    return newArr
+
+# Show output
+print(sortArr(arr))
